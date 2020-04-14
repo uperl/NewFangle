446da62afee1f3bf23f74d09916176b88848bf4b
Graham Ollis <plicease@cpan.org>
2020-04-14 18:37:28 -0400

    basics (probably not the final api)

--- lib/NewRelic.pm
+++ lib/NewRelic.pm
@@ -3,9 +3,19 @@ package NewRelic;
 use strict;
 use warnings;
 use 5.008001;
+use Alien::libnewrelic;
+use FFI::Platypus 1.00;
 
 # ABSTRACT: Unofficial Perl NewRelic SDK
 # VERSION
+
+my $ffi = FFI::Platypus->new(
+  api => 1,
+  lib => [Alien::libnewrelic->dynamic_libs],
+);
+
+$ffi->attach( newrelic_create_app_config  => ['string','string'] => 'opaque' );
+$ffi->attach( newrelic_destroy_app_config => ['opaque*'] => 'bool' );
 
 1;
 
