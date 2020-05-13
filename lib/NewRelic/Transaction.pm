5cb6fbee7d021c6c7b78f6304d95524b1c649abf
Graham Ollis <plicease@cpan.org>
2020-05-13 17:36:47 -0400

    add tansaction class

---
+++ lib/NewRelic/Transaction.pm
@@ -0,0 +1,59 @@
+package NewRelic::Transaction {
+
+  use strict;
+  use warnings;
+  use 5.020;
+  use NewRelic::FFI;
+  use Carp ();
+
+# ABSTRACT: NewRelic application class
+
+  $ffi->type('object(NewRelic::Transaction)' => 'newrelic_txn_t');
+
+=head1 SYNOPSIS
+
+ use NewRelic;
+ my $app = NewRelic::App->new;
+ $app->start_web_transaction("txn_name");
+
+=head1 DESCRIPTION
+
+NewRelic transaction class
+
+=head1 METHODS
+
+=head1 end
+
+ my $bool = $txn->end;
+
+Ends the transaction.
+
+=cut
+
+  $ffi->attach( [ end_transaction => 'end' ] => ['opaque*'] => 'bool' => sub {
+    my($xsub, $self) = @_;
+    my $ptr = $$self;
+    my $ret = $xsub->(\$ptr);
+    $$self = undef;
+    $ret;
+  });
+
+  sub DESTROY
+  {
+    my($self) = @_;
+    $self->end if defined $$self;
+  }
+
+};
+
+1;
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<NewRelic>
+
+=back
+
+=cut
