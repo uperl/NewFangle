d3f19440b6bbb29ad36a98d34efb96f7c924f063
Graham Ollis <plicease@cpan.org>
2020-05-14 20:53:21 -0600

    refactor

--- lib/NewFangle.pm
+++ lib/NewFangle.pm
@@ -4,7 +4,9 @@ package NewFangle {
   use warnings;
   use 5.020;
   use NewFangle::FFI;
-  use FFI::C 0.08;
+  use NewFangle::Config;
+  use NewFangle::App;
+  use NewFangle::CustomEvent;
   use base qw( Exporter );
 
 # ABSTRACT: Unofficial Perl NewRelic SDK
@@ -33,85 +35,6 @@ it is recommended that you review it before getting started.
 
 I've called this module L<NewFangle> in the hopes that one day NewRelic will write
 native Perl bindings and they can use the more obvious NewRelic namespace.
-
-=cut
-
-  FFI::C->ffi($ffi);
-
-  package NewFangle::NewrelicLoglevel {
-    FFI::C->enum([
-      'error',
-      'warning',
-      'info',
-      'debug',
-    ], { prefix => 'NEWRELIC_LOG_' });
-  }
-
-  package NewFangle::NewrelicTransactionTracerThreshold {
-    FFI::C->enum([
-      'is_apdex_failing',
-      'is_over_duration',
-    ], { prefix => 'NEWRELIC_THRESHOLD_' });
-  }
-
-  package NewFangle::NewrelicTtRecordsql {
-    FFI::C->enum([
-      'off',
-      'raw',
-      'obfuscated',
-    ], { prefix => 'NEWRELIC_SQL_' });
-  }
-
-  package NewFangle::DatastoreReporting {
-    FFI::C->struct([
-      enabled      => 'bool',
-      record_sql   => 'newrelic_tt_recordsql_t',
-      threshold_us => 'newrelic_time_us_t',
-    ]);
-  };
-
-  package NewFangle::NewrelicTransactionTracerConfig {
-    FFI::C->struct([
-      enabled                  => 'bool',
-      threshold                => 'newrelic_transaction_tracer_threshold_t',
-      duration_us              => 'newrelic_time_us_t',
-      stack_trace_threshold_us => 'newrelic_time_us_t',
-      datastore_reporting      => 'datastore_reporting_t',
-    ]);
-  }
-
-  package NewFangle::NewrelicDatastoreSegmentConfig {
-    FFI::C->struct([
-      instance_reporting      => 'bool',
-      database_name_reporting => 'bool',
-    ]);
-  }
-
-  package NewFangle::NewrelicDistributedTracingConfig {
-    FFI::C->struct([
-      enabled => 'bool',
-    ]);
-  }
-
-  package NewFangle::NewrelicSpanEventConfig {
-    FFI::C->struct([
-      enabled => 'bool',
-    ]);
-  }
-
-  package NewFangle::NewrelicAppConfig {
-    FFI::C->struct([
-      app_name            => 'string(255)',
-      license_key         => 'string(255)',
-      redirect_collector  => 'string(100)',
-      log_filename        => 'string(512)',
-      log_level           => 'newrelic_loglevel_t',
-      transaction_tracer  => 'newrelic_transaction_tracer_config_t',
-      datastore_tracer    => 'newrelic_datastore_segment_config_t',
-      distributed_tracing => 'newrelic_distributed_tracing_config_t',
-      span_events         => 'newrelic_span_event_config_t',
-    ], { trim_string => 1 });
-  }
 
 =head1 FUNCTIONS
 
@@ -162,10 +85,6 @@ Returns the
   $ffi->mangler(sub { "newrelic_$_[0]" });
 
   our @EXPORT_OK = grep /^newrelic_/, keys %NewFangle::;
-
-  require NewFangle::Config;
-  require NewFangle::App;
-  require NewFangle::CustomEvent;
 
 };
 
--- lib/NewFangle/FFI.pm
+++ lib/NewFangle/FFI.pm
@@ -5,6 +5,7 @@ package NewFangle::FFI {
   use 5.020;
   use FFI::CheckLib 0.27 ();
   use FFI::Platypus 1.26;
+  use FFI::C 0.08;
   use base qw( Exporter );
 
   our @EXPORT = qw( $ffi );
@@ -46,6 +47,83 @@ This is part of the internal workings for L<NewFangle>.
   $ffi->type('object(NewFangle::App)' => 'newrelic_app_t');
   $ffi->type('object(NewFangle::Transaction)' => 'newrelic_txn_t',);
   $ffi->type('object(NewFangle::CustomEvent)' => 'newrelic_custom_event_t');
+
+  FFI::C->ffi($ffi);
+
+  package NewFangle::NewrelicLoglevel {
+    FFI::C->enum([
+      'error',
+      'warning',
+      'info',
+      'debug',
+    ], { prefix => 'NEWRELIC_LOG_' });
+  }
+
+  package NewFangle::NewrelicTransactionTracerThreshold {
+    FFI::C->enum([
+      'is_apdex_failing',
+      'is_over_duration',
+    ], { prefix => 'NEWRELIC_THRESHOLD_' });
+  }
+
+  package NewFangle::NewrelicTtRecordsql {
+    FFI::C->enum([
+      'off',
+      'raw',
+      'obfuscated',
+    ], { prefix => 'NEWRELIC_SQL_' });
+  }
+
+  package NewFangle::DatastoreReporting {
+    FFI::C->struct([
+      enabled      => 'bool',
+      record_sql   => 'newrelic_tt_recordsql_t',
+      threshold_us => 'newrelic_time_us_t',
+    ]);
+  };
+
+  package NewFangle::NewrelicTransactionTracerConfig {
+    FFI::C->struct([
+      enabled                  => 'bool',
+      threshold                => 'newrelic_transaction_tracer_threshold_t',
+      duration_us              => 'newrelic_time_us_t',
+      stack_trace_threshold_us => 'newrelic_time_us_t',
+      datastore_reporting      => 'datastore_reporting_t',
+    ]);
+  }
+
+  package NewFangle::NewrelicDatastoreSegmentConfig {
+    FFI::C->struct([
+      instance_reporting      => 'bool',
+      database_name_reporting => 'bool',
+    ]);
+  }
+
+  package NewFangle::NewrelicDistributedTracingConfig {
+    FFI::C->struct([
+      enabled => 'bool',
+    ]);
+  }
+
+  package NewFangle::NewrelicSpanEventConfig {
+    FFI::C->struct([
+      enabled => 'bool',
+    ]);
+  }
+
+  package NewFangle::NewrelicAppConfig {
+    FFI::C->struct([
+      app_name            => 'string(255)',
+      license_key         => 'string(255)',
+      redirect_collector  => 'string(100)',
+      log_filename        => 'string(512)',
+      log_level           => 'newrelic_loglevel_t',
+      transaction_tracer  => 'newrelic_transaction_tracer_config_t',
+      datastore_tracer    => 'newrelic_datastore_segment_config_t',
+      distributed_tracing => 'newrelic_distributed_tracing_config_t',
+      span_events         => 'newrelic_span_event_config_t',
+    ], { trim_string => 1 });
+  }
 
 };
 
